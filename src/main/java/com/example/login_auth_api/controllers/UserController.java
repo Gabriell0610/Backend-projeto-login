936d5c8815fdb5210913e13360263071d6aece6b
Gabriell0610 <gabrielbarbosaa432@gmail.com>
2024-07-29 11:13:43 -0300

    feat: add lista de usuarios

--- src/main/java/com/example/login_auth_api/controllers/UserController.java
+++ src/main/java/com/example/login_auth_api/controllers/UserController.java
@@ -1,18 +1,27 @@
 package com.example.login_auth_api.controllers;
 
 
+import com.example.login_auth_api.domain.user.User;
+import com.example.login_auth_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserRepository repository;
+
+
     @GetMapping
-    public ResponseEntity<String> getUser() {
-        return ResponseEntity.ok("sucesso");
+    public List<User> getUser() {
+        return repository.findAll();
     }
 
 }
